3a6a93063088bca76e75219b474503099e8db473
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-10 21:10:10 -0300

    [ADD] adiciona componente search

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,17 @@
+import { MagnifyingGlass } from "phosphor-react";
+import { SearchFormContainer } from "./styles";
+
+export function SearchForm() {
+  return (
+    <div>
+      <SearchFormContainer>
+        <input placeholder="Busque por transações" />
+
+        <button type="submit">
+          <MagnifyingGlass size={20} />
+          Buscar
+        </button>
+      </SearchFormContainer>
+    </div>
+  );
+}
