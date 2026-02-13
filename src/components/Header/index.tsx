42b6fb7e3b2ba746fee34466d25c3adb8f207b20
guilherme1s <2000guilherme.silva@gmail.com>
2026-02-13 20:16:42 -0300

    [ADD] adiciona modal de transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransationsModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -7,7 +9,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+         <NewTransationsModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
